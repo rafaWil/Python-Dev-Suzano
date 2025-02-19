4989bea34baa5129d986c56f04eb2a5a0c3be47d
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-19 02:05:20 -0300

    feat: diferentes operadores nos print

--- tipos-operadores/opAritmeticos/opAritmeticos.py
+++ tipos-operadores/opAritmeticos/opAritmeticos.py
@@ -2,3 +2,7 @@ produto_1 = 10
 produto_2 = 20
 
 print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
