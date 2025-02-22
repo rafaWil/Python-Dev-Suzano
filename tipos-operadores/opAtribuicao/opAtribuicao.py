7ae41e32ecb6c5b79b16213292b9a562c0be4643
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-22 14:59:49 -0300

    feat: Atribuicao com Adicao

--- tipos-operadores/opAtribuicao/opAtribuicao.py
+++ tipos-operadores/opAtribuicao/opAtribuicao.py
@@ -5,3 +5,11 @@ valor_armazenado = 5
 print(valor_armazenado)
 >>> resultado -> 5
 """
+
+# Atribuição com Adição 
+"""
+Exemplo:
+valor_armazenado = 5
+valor_armazenado += 10
+print(valor_armazenado)
+"""
