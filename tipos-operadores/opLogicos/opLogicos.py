685ab57d0a7b6aed0b43565a351181acf257ae69
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-22 15:27:49 -0300

    feat: Apenas exemplo do uso de comparacao

---
+++ tipos-operadores/opLogicos/opLogicos.py
@@ -0,0 +1,13 @@
+"""
+Exemplo de Comparacao 
+saldo = 1000
+saque = 200
+limite = 100
+
+print(saldo >= saque) 
+# >>> Resultado deve ser TRUE
+
+print(saque <= limite)
+# >>> Resultado deve ser FALSE
+"""
+
