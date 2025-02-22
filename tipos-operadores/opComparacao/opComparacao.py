ad7721956caec65c18d5a1cd0c26cc4cba26092b
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-22 13:00:52 -0300

    feat: Print de teste nas operacoes de comparacao

--- tipos-operadores/opComparacao/opComparacao.py
+++ tipos-operadores/opComparacao/opComparacao.py
@@ -1,3 +1,9 @@
 # Variaveis com valores para exemplo
 saldo = 200
 saque = 200
+
+# Print para teste de Op. Comparacao
+print("Resultado se saldo é diferente de saque -> ", saldo != saque)
+print("Resultado se saldo é igual a saque -> ", saldo == saque)
+print("Resultado se saldo é maior que saque -> ", saldo  >= saque)
+print("Resultado se saldo é menor que saque -> ", saldo <= saque)
