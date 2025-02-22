cd6ff0a3f229f412b1e2bd30e07abca3c25e427a
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-22 15:03:03 -0300

    feat: Outras Operacoes de Atribuicao

--- tipos-operadores/opAtribuicao/opAtribuicao.py
+++ tipos-operadores/opAtribuicao/opAtribuicao.py
@@ -5,7 +5,6 @@ valor_armazenado = 5
 print(valor_armazenado)
 >>> resultado -> 5
 """
-
 # Atribuição com Adição 
 """
 Exemplo:
@@ -13,4 +12,52 @@ valor_armazenado = 5
 valor_armazenado += 10
 print(valor_armazenado)
 >>> Resultado -> 15
+"""
+# Atribuição com Subtração
+"""
+Exemplo:
+valor_armazenado = 15
+valor_armazenado -= 5
+print(valor_armazenado)
+>>> Resultado -> 10
+"""
+# Atribuição com Multiplicação
+"""
+Exemplo:
+valor_armazenado = 5
+valor_armazenado *= 3
+print(valor_armazenado)
+>>> Resultado -> 15
+"""
+# Atribuição com Divisão
+"""
+Exemplo:
+valor_armazenado = 15
+valor_armazenado /= 3
+print(valor_armazenado)
+>>> Resultado -> 5.0
+"""
+# Atribuição com Módulo (Resto da Divisão)
+"""
+Exemplo:
+valor_armazenado = 15
+valor_armazenado %= 3
+print(valor_armazenado)
+>>> Resultado -> 0
+"""
+# Atribuição com Potenciação
+"""
+Exemplo:
+valor_armazenado = 5
+valor_armazenado **= 3
+print(valor_armazenado)
+>>> Resultado -> 125
+"""
+# Atribuição com Raiz Quadrada
+"""
+Exemplo:
+valor_armazenado = 25
+valor_armazenado **= 0.5
+print(valor_armazenado)
+>>> Resultado -> 5.0
 """
