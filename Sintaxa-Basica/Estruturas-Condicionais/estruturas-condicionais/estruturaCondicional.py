b1e9b2c0de9ba8d511ed6b76277b7be45604095a
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 17:26:18 -0300

    feat: Exemplo com IF

--- Sintaxa-Basica/Estruturas-Condicionais/estruturas-condicionais/estruturaCondicional.py
+++ Sintaxa-Basica/Estruturas-Condicionais/estruturas-condicionais/estruturaCondicional.py
@@ -1,2 +1,12 @@
 # Oque são?
 # Permite o desvio de fluxo de controle quando determinadas expressões lógicas são atendidas.
+
+# Exemplo com IF
+saldo = 2000.0
+saque = float(input("Informe o valor de saque: "))
+
+if  saldo >= saque:
+    print("Realizando Saque...")
+
+if saldo <= saque:
+    print("Saldo Insuficiente!")
