c880bd8c3c6e3e6fa43f895426663ad3297a9e0b
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 17:18:51 -0300

    feat: Comentario explicando Indentacao

---
+++ Sintaxa-Basica/Estruturas-Condicionais/Indetacao-Blocos/indetacaoBlocos.py
@@ -0,0 +1,5 @@
+# Oque é Indentar?
+"""
+    Identar código é uma forma de manter o código fonte mais legível e manutenível.
+    Mas em Python ela exerce um papel de determinar onde um bloco de comando inicia e onde ele termina.
+"""
