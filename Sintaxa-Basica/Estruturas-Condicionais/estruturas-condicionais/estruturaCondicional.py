f8014754d25ac2e00f828ff5ca0d4b9c594b9cde
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 17:41:00 -0300

    feat: Exemplo com IF ELIF ELSE

--- Sintaxa-Basica/Estruturas-Condicionais/estruturas-condicionais/estruturaCondicional.py
+++ Sintaxa-Basica/Estruturas-Condicionais/estruturas-condicionais/estruturaCondicional.py
@@ -1,6 +1,10 @@
 # Oque são?
 # Permite o desvio de fluxo de controle quando determinadas expressões lógicas são atendidas.
 
+# Bibliotecas OS e SYS
+import os
+import sys
+
 # Exemplo com IF
 saldo = 2000.0
 saque = float(input("Informe o valor de saque: "))
@@ -19,3 +23,14 @@ if  saldo >= saque:
     print("Realizando Saque...")
 else:
     print("Saldo Insuficiente!")
+    
+# Exemplo com IF ELIF ELSE
+opcao = int(input("Informe uma opção: [1] Sacar \n[2] Extrato: "))
+
+if opcao == 1:
+    valor = float(input("Informe a quantia para o saque: "))
+    print("Realizando saque no valor de ", valor, "...")
+elif opcao == 2:
+    print("Exibindo Extrato...")
+else:
+    sys.exit("Opcao Invalida!")
