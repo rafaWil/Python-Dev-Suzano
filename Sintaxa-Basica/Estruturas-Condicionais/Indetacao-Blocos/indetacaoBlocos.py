30762c2346dfbe23cc4f3ed3fa14d6cf3160ade8
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 17:21:00 -0300

    feat: Exemplo de Bloco Indentado em Python

--- Sintaxa-Basica/Estruturas-Condicionais/Indetacao-Blocos/indetacaoBlocos.py
+++ Sintaxa-Basica/Estruturas-Condicionais/Indetacao-Blocos/indetacaoBlocos.py
@@ -3,3 +3,10 @@
     Identar código é uma forma de manter o código fonte mais legível e manutenível.
     Mas em Python ela exerce um papel de determinar onde um bloco de comando inicia e onde ele termina.
 """
+
+# Exemplo de Bloco em Python
+def sacar(self, valor: float) -> None: # Inicio do bloco do método
+    if self.saldo >= valor: # Inicio do bloco do if
+        self.saldo -= valor # Inicio do bloco da instrução
+    # Fim do bloco do if
+# Fim do bloco do método
