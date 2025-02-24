ac6e775398663ad771e53dc427c55c751ea7e850
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 18:14:35 -0300

    feat: Explicando oque é Estrutura de Repeticao

---
+++ Sintaxa-Basica/Estruturas-Condicionais/estruturas-repeticao/estrutRepeticao.py
@@ -0,0 +1,8 @@
+# Oque são Estruturas de Repetição
+
+# Estruturas de repetição são usadas para executar um bloco de código várias
+# vezes. Elas são usadas para processar coleções de dados, como listas,
+# tuplas, dicionários, conjuntos, etc.
+# Existem dois tipos de estruturas de repetição em Python: for e while.
+# O for é usado para iterar sobre uma coleção de dados, enquanto o while é
+# usado para executar um bloco de código enquanto uma condição for verdadeira.
