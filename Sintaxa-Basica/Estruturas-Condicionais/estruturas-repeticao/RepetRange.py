bdd8e6a70fe36fbb71c4fb2ebddaae7ee1803e96
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 21:21:09 -0300

    feat: Funcao RANGE oque é?

---
+++ Sintaxa-Basica/Estruturas-Condicionais/estruturas-repeticao/RepetRange.py
@@ -0,0 +1,9 @@
+# Função Range
+
+"""
+    Range é uma função built-in do Python, ela é usada para produzir uma sequencia de numeros inteiros
+    a partir de um inicio(inclusivo) para um fim(exclusivo). Se usarmos range(i,j) será produzido:
+    
+    i, i+1, i+2, i+3,....j-1.
+    Ela recebe 3 argumentos: STOP(Obrigatório), START(Opcional) e STEP OPCIONAL.
+"""
