418f2bf9acd3f000dcd52fdfe952a965b5542f99
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 21:26:37 -0300

    feat: Exemplo com a tabuada de 5

--- Sintaxa-Basica/Estruturas-Condicionais/estruturas-repeticao/RepetRange.py
+++ Sintaxa-Basica/Estruturas-Condicionais/estruturas-repeticao/RepetRange.py
@@ -8,7 +8,12 @@
     Ela recebe 3 argumentos: STOP(Obrigatório), START(Opcional) e STEP OPCIONAL.
 """
 
-# Utilizando range como FOR
+# Utilizando range com FOR
 for numero in range(0,11):
     print(numero, end=" ")
+print("\n")
 # Resultado 0 1 2 3 4 5 6 7 8 9 10
+
+# Outro exemplo -> Tabuada de 5
+for num in range(0, 51, 5):
+    print(num, end=" ")
