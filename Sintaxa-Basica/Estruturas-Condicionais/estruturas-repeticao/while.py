0d4a5f03841f036de19fb2d8d08231f094756a55
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-24 21:37:10 -0300

    feat: Exemplo de uso do while

--- Sintaxa-Basica/Estruturas-Condicionais/estruturas-repeticao/while.py
+++ Sintaxa-Basica/Estruturas-Condicionais/estruturas-repeticao/while.py
@@ -1,3 +1,12 @@
 # WHILE
-
 # O comando WHILE é usado para repetir um bloco de código várias vezes.
+
+# Exemplo:
+opcao = -1
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: ")) 
+    
+    if opcao == 1:
+        print("Sacando...")
+    elif opcao == 2:
+        print("Exibindo o Exrato")
