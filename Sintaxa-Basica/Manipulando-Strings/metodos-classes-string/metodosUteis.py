68aeb79dbd97856cedcad2956d3f00f648ae53cb
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-26 13:35:15 -0300

    feat: Transformando no print o texto da var curso em Maiusculo, minusculo e em titulo

--- Sintaxa-Basica/Manipulando-Strings/metodos-classes-string/metodosUteis.py
+++ Sintaxa-Basica/Manipulando-Strings/metodos-classes-string/metodosUteis.py
@@ -3,3 +3,14 @@
 # Variavel com letras de tamanho diferente
 curso = "pYtHon"
 
+# Transformando em Maiuscula
+print(curso.upper())
+# >>> Resultado -> PYTHON
+
+# Transformando em Minuscula
+print(curso.lower())
+# >>> Resultado -> python
+
+# Transformando em Titulo
+print(curso.title())
+# >>> Resultado -> Python
