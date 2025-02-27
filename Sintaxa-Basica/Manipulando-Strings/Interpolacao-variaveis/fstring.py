995ea0d5a931c43caf6893e7cc2955ef03ad6b99
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-27 17:05:49 -0300

    feat: Metodo F-String

---
+++ Sintaxa-Basica/Manipulando-Strings/Interpolacao-variaveis/fstring.py
@@ -0,0 +1,9 @@
+# Metodo F-STRING
+
+nome = input("Insira seu nome: ") 
+idade = int(input("Insira sua idade: "))
+profissao = input("Profissao: ")
+linguagem = input("Qual linguagem principal de trabalho: ")
+
+# Print com metodo F-String
+print(f"Olá, {nome}! Você tem {idade} anos e é {profissao} e trabalha com a linguagem de programacao {linguagem}")
