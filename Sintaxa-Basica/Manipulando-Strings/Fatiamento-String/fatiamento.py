de1e47ccf82d6fa942851d9f703cc37913712632
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-27 17:11:42 -0300

    feat: Exemplo de Fatiamento

---
+++ Sintaxa-Basica/Manipulando-Strings/Fatiamento-String/fatiamento.py
@@ -0,0 +1,9 @@
+# Exemplo de fatiamento
+
+variavelExemplo = "Wilson Rafael Gomes Melo"
+
+print(variavelExemplo[0])
+# >>> Resultado: "W"    
+
+print(variavelExemplo[:6])
+# >>> Resultado: "Wilson"
