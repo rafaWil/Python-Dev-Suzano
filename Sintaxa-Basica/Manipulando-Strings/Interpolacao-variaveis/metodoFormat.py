76ba8abf56d2af553c6cbd06b4d804c69dbab192
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-27 17:02:21 -0300

    feat: Metodo Format

---
+++ Sintaxa-Basica/Manipulando-Strings/Interpolacao-variaveis/metodoFormat.py
@@ -0,0 +1,9 @@
+# Metodo FORMAT
+
+nome = input("Insira seu nome: ") 
+idade = int(input("Insira sua idade: "))
+profissao = input("Profissao: ")
+linguagem = input("Qual linguagem principal de trabalho: ")
+
+# Print usando metodo format
+print("Meu nome é {} e tenho {} anos, sou {} e trabalho com {}".format(nome, idade, profissao, linguagem))
