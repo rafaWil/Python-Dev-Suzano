1e1d1081d2dd9971454969560844ba1b5cfa55e1
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-28 16:40:56 -0300

    feat: Strings TRIPLAS com exemplo de texto

--- Sintaxa-Basica/Manipulando-Strings/multiplasLinhas/MultiplasLinhas.py
+++ Sintaxa-Basica/Manipulando-Strings/multiplasLinhas/MultiplasLinhas.py
@@ -3,3 +3,13 @@
     String de Múltiplas Linhas são definidas informando 3 aspas simples ou duplas durante a atribuição. Elas
     podem ocupar várias linhas do código, e todos os espaços em branco são incluídos na string final.
 """
+
+# Strings TRIPLAS
+nome = "Wilson"
+
+mensagem = f'''
+Olá meu nome é {nome},
+    Eu sou um desenvolvedor Python.
+'''
+# >>> RESULTADO: Olá meu nome é Wilson,
+                # Eu sou um desenvolvedor Python.
