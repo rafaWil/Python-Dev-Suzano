fee0b137c3573a24105e9a3e731ee77c77e67dc6
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-02-28 16:38:16 -0300

    feat: oque é String de Multiplas Linhas?

---
+++ Sintaxa-Basica/Manipulando-Strings/multiplasLinhas/MultiplasLinhas.py
@@ -0,0 +1,5 @@
+# OQUE É STRING DE MULTIPLAS LINHAS
+"""
+    String de Múltiplas Linhas são definidas informando 3 aspas simples ou duplas durante a atribuição. Elas
+    podem ocupar várias linhas do código, e todos os espaços em branco são incluídos na string final.
+"""
