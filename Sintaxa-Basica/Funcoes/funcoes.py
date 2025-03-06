8e11fbc4d0fa569e8488306440302338f401406f
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-05 21:00:22 -0300

    feat: Explicando oque seria FUNCOES

---
+++ Sintaxa-Basica/Funcoes/funcoes.py
@@ -0,0 +1,8 @@
+"""
+    FUNÇÕES?
+    
+    Função é um bloco de código identificado por um nome e pode receber uma lista de parâmetros, esses 
+    paramêtros podem ou não ter valores padrões. Usar funções torna o código mais legível e possibilita
+    o reaproveitamento de código. Programar baseado em funções, é o mesmo que dizer que estamos programando
+    de maneira estruturada.
+"""
