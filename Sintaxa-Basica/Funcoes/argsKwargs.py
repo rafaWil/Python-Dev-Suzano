14b1368999daf8a5bf344907ceb0d2ac49943d49
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-05 22:14:54 -0300

    feat: Exemplo

--- Sintaxa-Basica/Funcoes/argsKwargs.py
+++ Sintaxa-Basica/Funcoes/argsKwargs.py
@@ -4,3 +4,12 @@
     Podemos combinar parametros obrigatorios com args e kwargs. Quando esses sao definidos
     (*args e **kwargs), o metodo recebe os valores como tupla e dicionario respectivamente.
 """
+
+# Exemplo
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+    
+exibir_poema("Zen of Python", "Beautiful is better than ugly.", autor = "Tim Peters", ano = 1999)
