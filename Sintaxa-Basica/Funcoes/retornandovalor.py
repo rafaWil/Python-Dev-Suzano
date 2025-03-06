2a32342d8703e668238f3ee05d36897059d9e2e2
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-05 21:32:50 -0300

    Docs/Feat: Arquivo de continuacao de aula e explicacao RETORNANDO VALORES

---
+++ Sintaxa-Basica/Funcoes/retornandovalor.py
@@ -0,0 +1,7 @@
+"""
+    RETORNANDO VALORES
+    
+    Para retornar um valor, utilizamos a palavra reservada RETURN.    
+    Toda funcao Python retorna NONE por padrao. Diferente de outras linguagens de programaçao,
+    em Python uma funcao pode retornar mais de um valor.
+"""
