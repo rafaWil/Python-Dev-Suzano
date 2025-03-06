a422225ea5d069b27430b1c72b5948e70a5b7120
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-05 22:04:00 -0300

    feat: Exemplo de retorno de valor

--- Sintaxa-Basica/Funcoes/retornandovalor.py
+++ Sintaxa-Basica/Funcoes/retornandovalor.py
@@ -5,3 +5,18 @@
     Toda funcao Python retorna NONE por padrao. Diferente de outras linguagens de programaçao,
     em Python uma funcao pode retornar mais de um valor.
 """
+
+# EXEMPLO
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+    return antecessor, sucessor
+
+calcular_total([10, 20, 34]) # 64
+antecessor, sucessor = retorna_antecessor_e_sucessor(10) # (9, 11)
+
+print(antecessor, sucessor)
