3ff738c1289f3ea6b1c786513117cb1d5f76685e
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-05 21:07:14 -0300

    feat: Exemplo de funcoes

--- Sintaxa-Basica/Funcoes/funcoes.py
+++ Sintaxa-Basica/Funcoes/funcoes.py
@@ -6,3 +6,18 @@
     o reaproveitamento de código. Programar baseado em funções, é o mesmo que dizer que estamos programando
     de maneira estruturada.
 """
+
+# Exemplo
+def exibir_mensagem():
+    print("Olá, mundo!")
+    
+def exibir_mensagem_2(nome):
+    print(f"Olá, {nome}!")
+    
+def exibir_mensagem_3(nome = "Anônimo"):
+    print(f"Olá, {nome}!")
+    
+exibir_mensagem()
+exibir_mensagem_2(nome = "Guilherme")
+exibir_mensagem_3()
+exibir_mensagem_3(nome = "Chappie") 
