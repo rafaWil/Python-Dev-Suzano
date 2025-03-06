a5eabdde7e5a7c8789acd265bce45993cb3c9e6b
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-05 22:10:50 -0300

    feat: oque seria o args e kwargs

---
+++ Sintaxa-Basica/Funcoes/argsKwargs.py
@@ -0,0 +1,6 @@
+"""
+    ARGS e KWARGS
+    
+    Podemos combinar parametros obrigatorios com args e kwargs. Quando esses sao definidos
+    (*args e **kwargs), o metodo recebe os valores como tupla e dicionario respectivamente.
+"""
