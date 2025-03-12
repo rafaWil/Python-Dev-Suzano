6a18b5a059eb355a199bbec9888429c5950a3984
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 15:23:12 -0300

    Feat: Lista Aninhada

--- Colecoes/Listas/criacaoAcesssoDados/acessoDireto.py
+++ Colecoes/Listas/criacaoAcesssoDados/acessoDireto.py
@@ -10,3 +10,16 @@ print(frutas[0])  # Acessa a primeira posição da lista (maçã)
 print(frutas[3])  # Acessa a quarta posição da lista (morango)
 
 print(frutas[-1]) # Acessa a última posição da lista (morango)
+
+
+# Exemplo lista aninhadas
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [5, 6, "c"]
+]
+
+matriz[0] # [1, "a", 2]
+matriz[0][0] # 1
+matriz[0][-1] # 2
+matriz[-1][-1] # "c"
