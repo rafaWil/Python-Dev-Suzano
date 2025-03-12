6e95cb4830dbd2912c901cd50110107da4b81ba6
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 17:01:18 -0300

    Feat: Inserindo metodo id para verificar valores de identificacao das listas

--- Colecoes/Listas/metodosList/metodos.py
+++ Colecoes/Listas/metodosList/metodos.py
@@ -1,8 +1,5 @@
 # APPEND
-"""
-    OQUE É?
-    é um método usado para adicionar um elemento ao final de uma lista.
-"""
+    # é um método usado para adicionar um elemento ao final de uma lista.
 
 # Exemplo
 lista = []
@@ -14,3 +11,12 @@ lista.append([40, 30, 20])
 print(lista) # Resultado => [1, "Python", [40, 30, 20]]
 
 # COPY
+lista2 = [1, "Python", [40,30,20]]
+
+# Não é a mesma lista
+lista2.copy()
+print(lista2) # Resultado => [1, "Python", [40, 30,20]] 
+
+# Para se certificar de que sao listas diferentes, vemos pelo id
+print(id(lista2)) 
+print(id(lista.copy())) 
