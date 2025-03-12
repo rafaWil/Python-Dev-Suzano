bff2f62806cc80eee40f9bc2ac4bf0d37b6aafce
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 15:17:25 -0300

    Feat: Acesso Direto

---
+++ Colecoes/Listas/criacaoAcesssoDados/acessoDireto.py
@@ -0,0 +1,5 @@
+"""
+    ACESSO DIRETO
+    
+    >> A lista é uma sequencia, portanto podemos acessar seus dados utilizando indices. Contamos o indice de determinada sequencia a partir de zero.
+"""
