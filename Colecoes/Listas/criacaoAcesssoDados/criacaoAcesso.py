67f5f33f64a535cdfa2ef8222bbbc2a97d61f62a
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 15:13:35 -0300

    Feat: Calling JSON and include examples about list types

--- Colecoes/Listas/criacaoAcesssoDados/criacaoAcesso.py
+++ Colecoes/Listas/criacaoAcesssoDados/criacaoAcesso.py
@@ -1,6 +1,21 @@
 """
     LISTAS
 
-    >> Podem armazenar de maneira sequencial qualquer tipo de objeto. Podemos criar listas utilizando o construtor List, a função range ou colocando valores separados por vírgula dentro de colchetes. Listas são objetos mutáveis, portanto podemos alterar seus valores após a criação.
+    >> Podem armazenar de maneira sequencial qualquer tipo de objeto. Podemos criar listas utilizando o c<nstrutor List, a função range ou colocando valores separados por vírgula dentro de colchetes. Listas são objetos mutáveis, portanto podemos alterar seus valores após a criação.
     >> list
 """
+
+# Exemplo de lista com colchetes e valores
+frutas = ["laranja", "maca", "uva"]
+
+# Exemplo de lista com colchetes e valor vazio
+frutas_vazia = []
+
+# Variavel declarando com construtor list
+letras = list("python")
+
+# Usando range para list e criar um elemento para cada
+numeros = list(range(10))
+
+# Exemplo com lista com todos atributos de um carro
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
