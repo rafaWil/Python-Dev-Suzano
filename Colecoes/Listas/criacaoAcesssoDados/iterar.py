0f3b6543b611dc80c2082acbdfbd91c3eb9e86d6
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 15:38:15 -0300

    Feat: For para iterar em uma lista

---
+++ Colecoes/Listas/criacaoAcesssoDados/iterar.py
@@ -0,0 +1,11 @@
+"""
+    ITERAR 
+    
+    >> A forma mais comum para percorrer os dados de uma lista é utilizando o comando FOR.
+"""
+
+# Exemplo
+carros = ["Porsche", "BMW", "Civic", "Ferrari"]
+
+for c in carros:
+    print(c)
