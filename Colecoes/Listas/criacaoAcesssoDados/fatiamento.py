3487af571b73d0b4994f969ae821a0244af0b8e0
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 15:25:18 -0300

    Feat: Fatiamento explicacao

---
+++ Colecoes/Listas/criacaoAcesssoDados/fatiamento.py
@@ -0,0 +1,5 @@
+"""
+    FATIAMENTO
+    
+    >> Alem de acessar elementos diretamente, podemos extrair um conjunto de valores de uma sequencia. Para isso basta passar o indice inicial e/ou final para acessar o conjunto. Podemos ainda informar quantas posicoes o cursor deve "pular" no acesso.
+"""
