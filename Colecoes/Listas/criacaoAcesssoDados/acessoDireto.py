b9de7272054942e12db66896f9903807fc005f49
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 15:18:24 -0300

    Feat: Exemplo com lista de frutas

--- Colecoes/Listas/criacaoAcesssoDados/acessoDireto.py
+++ Colecoes/Listas/criacaoAcesssoDados/acessoDireto.py
@@ -3,3 +3,8 @@
     
     >> A lista é uma sequencia, portanto podemos acessar seus dados utilizando indices. Contamos o indice de determinada sequencia a partir de zero.
 """
+
+# Exemplo com lista de frutas
+frutas = ['maçã', 'banana', 'laranja', 'morango']
+print(frutas[0])  # Acessa a primeira posição da lista (maçã)
+print(frutas[3])  # Acessa a quarta posição da lista (morango)
