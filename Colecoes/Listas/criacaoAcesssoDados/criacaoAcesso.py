cff75d97cc76f9105adf8f2b7e4f6dc69e02d4a8
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 14:59:32 -0300

    Feat: Explicando oque seria LISTAS

---
+++ Colecoes/Listas/criacaoAcesssoDados/criacaoAcesso.py
@@ -0,0 +1,6 @@
+"""
+    LISTAS
+
+    >> Podem armazenar de maneira sequencial qualquer tipo de objeto. Podemos criar listas utilizando o construtor List, a função range ou colocando valores separados por vírgula dentro de colchetes. Listas são objetos mutáveis, portanto podemos alterar seus valores após a criação.
+    >> list
+"""
