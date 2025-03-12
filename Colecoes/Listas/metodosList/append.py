da048ce2737884ecb925fc4569c1e65c56e05cc7
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-12 16:57:00 -0300

    Feat: Append e exemplo de uso

---
+++ Colecoes/Listas/metodosList/append.py
@@ -0,0 +1,14 @@
+# APPEND
+"""
+    OQUE É?
+    é um método usado para adicionar um elemento ao final de uma lista.
+"""
+
+# Exemplo
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista) # Resultado => [1, "Python", [40, 30, 20]]
