c805d88c8a4696d34c2b2019d2b6b8096b9e4f56
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 19:01:03 -0300

    Feat: CLEAR

---
+++ Colecoes/Dicionarios/metodos.py
@@ -0,0 +1,10 @@
+# CLEAR
+contatos = {
+    "wilson@gmail.com": {"nome":"wilson", "telefone":"0000-0000"},
+    "joao@gmail.com": {"nome":"João", "telefone":"1111-111"},
+    "david@gmail.com": {"nome":"David", "telefone":"2222-2222"},
+    "raquel@gmail.com": {"nome":"Raquel", "telefone":"4444-4444"},
+}
+
+contatos.clear()
+contatos # {}
