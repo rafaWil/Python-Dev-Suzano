16e169cef778d489c82afe1558322a9dcb1fa5f3
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 11:37:42 -0300

    Feat: Acessando os dados

--- Colecoes/Conjuntos/conjuntos.py
+++ Colecoes/Conjuntos/conjuntos.py
@@ -7,3 +7,9 @@
 set([1, 2, 3, 1, 3, 4]) # {1,2,3,4}
 
 set("abacaxi")  # {"b", "a", "c", "x", "i"}
+
+
+# ACESSANDO OS DADOS
+"""
+    >> Conjuntos em Python não suportam indexação e nem fatiamento, caso queira acessar os seus valores é necessário converter o conjunto para lista.
+"""
