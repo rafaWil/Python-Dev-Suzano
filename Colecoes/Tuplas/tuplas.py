d04fb05673495ef4ec420142505646f6b6da5329
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 10:42:33 -0300

    Feat: Oque são TUPLAS

---
+++ Colecoes/Tuplas/tuplas.py
@@ -0,0 +1,6 @@
+# CRIANDO TUPLAS
+"""
+    >> Tuplas são estruturas de dados muito parecidas com as listas, a principal diferença é que tuplas
+    são imutáveis enquanto listas são mutáveis. Podemos criar tuplas através da classe TUPLE, ou colocando
+    
+"""
