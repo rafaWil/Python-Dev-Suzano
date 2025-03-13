92ab29cb1229634148f06345d704e030c96984c9
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 11:12:38 -0300

    Feat: Criando SETS

---
+++ Colecoes/Conjuntos/conjuntos.py
@@ -0,0 +1,4 @@
+# CRIANDO SETS
+"""
+    >> Um set é uma coleção que não possui objetos repetidos, usamos sets para representar conjuntos matemáticos ou eliminar itens duplicados de um iterável.
+"""
