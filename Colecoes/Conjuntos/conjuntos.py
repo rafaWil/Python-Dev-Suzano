91af4f43e1cdb33bb8a003fd591a224357b85778
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 11:33:38 -0300

    Feat: Exemplos usando SET

--- Colecoes/Conjuntos/conjuntos.py
+++ Colecoes/Conjuntos/conjuntos.py
@@ -2,3 +2,8 @@
 """
     >> Um set é uma coleção que não possui objetos repetidos, usamos sets para representar conjuntos matemáticos ou eliminar itens duplicados de um iterável.
 """
+
+# Exemplo
+set([1, 2, 3, 1, 3, 4]) # {1,2,3,4}
+
+set("abacaxi")  # {"b", "a", "c", "x", "i"}
