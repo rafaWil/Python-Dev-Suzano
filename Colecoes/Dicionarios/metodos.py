d81aaeff20b6f4c12a9c3a7f9b059d6c86a160ee
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 19:16:58 -0300

    Feat: Metodo COPY

--- Colecoes/Dicionarios/metodos.py
+++ Colecoes/Dicionarios/metodos.py
@@ -7,4 +7,15 @@ contatos = {
 }
 
 contatos.clear()
-contatos # {}
+contatos # {} Apagando todos os valores
+
+
+# COPY
+contatos = {
+    "wilson@gmail.com": {"nome":"wilson", "telefone":"0000-0000"},
+    "joao@gmail.com": {"nome":"João", "telefone":"1111-111"},
+    "david@gmail.com": {"nome":"David", "telefone":"2222-2222"},
+    "raquel@gmail.com": {"nome":"Raquel", "telefone":"4444-4444"},
+}
+contatos_copia = contatos.copy()
+
