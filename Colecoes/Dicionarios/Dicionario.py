956f1c29479d239057aae5e7c050ffafafbad34d
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 15:49:14 -0300

    Feat: Oq sao DICIONARIOS

---
+++ Colecoes/Dicionarios/Dicionario.py
@@ -0,0 +1,5 @@
+# DICIONARIOS
+
+"""
+    ==> Um dicionario é um conjunto não-ordenado de pares chave:valor, onde as chaves são únicas em uma dada instância do dicionário. Dicionários são delimitados por chaves: {}, e contém uma lista de pares chave: valor separada por vírgulas.
+""" 
