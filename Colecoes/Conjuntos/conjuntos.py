fc2216d6687ba08b313b6706ddc1edcb8b6f12b3
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-13 11:47:50 -0300

    Feat: Metodos de set

--- Colecoes/Conjuntos/conjuntos.py
+++ Colecoes/Conjuntos/conjuntos.py
@@ -13,3 +13,22 @@ set("abacaxi")  # {"b", "a", "c", "x", "i"}
 """
     >> Conjuntos em Python não suportam indexação e nem fatiamento, caso queira acessar os seus valores é necessário converter o conjunto para lista.
 """
+
+# METODOS DA CLASSE SET
+# 1. add() - Adiciona um elemento ao conjunto
+# 2. discard() - Remove um elemento do conjunto, caso ele exista
+# 3. pop() - Remove um elemento do conjunto, caso ele exista, e retorna
+# 4. remove() - Remove um elemento do conjunto, caso ele exista
+# 5. union() - Retorna um conjunto com todos os elementos dos conjuntos
+# 6. intersection() - Retorna um conjunto com os elementos em comum entre os conjuntos
+# 7. difference() - Retorna um conjunto com os elementos que estão em um conjunto e não 
+# 8. symmetric_difference() - Retorna um conjunto com os elementos que estão em um conjunto 
+# 9. isdisjoint() - Verifica se dois conjuntos são disjuntos
+# 10. issuperset() - Verifica se um conjunto é superset de outro conjunto
+# 11. issubset() - Verifica se um conjunto é subset de outro conjunto
+# 12. copy() - Retorna uma cópia do conjunto
+# 13. clear() - Remove todos os elementos do conjunto
+# 14. len() - Retorna o tamanho do conjunto
+# 15. update() - Atualiza o conjunto com os elementos de outro conjunto
+# 16. union() - Retorna um conjunto com todos os elementos dos conjuntos
+# 18. difference() - Retorna um conjunto com os elementos que estão em um conjunto e não
