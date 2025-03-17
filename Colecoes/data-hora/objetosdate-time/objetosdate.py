9faf72e567488ce7ada8324c9b12aae1d0741f56
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-17 10:06:15 -0300

    Feat: Exemplo de uso da funcao datetime

--- Colecoes/data-hora/objetosdate-time/objetosdate.py
+++ Colecoes/data-hora/objetosdate-time/objetosdate.py
@@ -3,3 +3,8 @@
 """
     É usado para lidar com datas e horas. Ele possui várias classes úteis como DATE, TIME e TIMEDELTA.
 """
+# Exemplo
+import datetime # Biblioteca para uso de data/hora
+
+data = datetime.date(2025, 3, 17)
+print(data) # => Resultado 2025-03-17
