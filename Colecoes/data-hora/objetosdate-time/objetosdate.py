8da40592e086443fb38b6ed95e76f8750d9a1b4b
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-17 10:17:41 -0300

    Feat: Retornando data atual -> Date.Today

--- Colecoes/data-hora/objetosdate-time/objetosdate.py
+++ Colecoes/data-hora/objetosdate-time/objetosdate.py
@@ -6,5 +6,9 @@
 # Exemplo
 import datetime # Biblioteca para uso de data/hora
 
-data = datetime.date(2025, 3, 17)
+data = datetime.date(2022, 5, 17)
 print(data) # => Resultado 2025-03-17
+
+# retornando data atual 
+data_atual = datetime.date.today()
+print(data_atual) # => Resultado data atual
