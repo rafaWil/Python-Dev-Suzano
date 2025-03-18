cf0ba0249ec51637d8a0fb3b99bddc7ccf267392
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-17 22:31:30 -0300

    Feat: Inserindo hora e dia da semana

--- Colecoes/data-hora/formatando-convert-datas/exemplo.py
+++ Colecoes/data-hora/formatando-convert-datas/exemplo.py
@@ -2,7 +2,7 @@ from datetime import date, time, datetime, timedelta, timezone
 
 data_hora_atual = datetime.now()
 data_hora_str = "2023-10-20 10:20"
-mascara_ptbr = "%d/%m/%Y"
+mascara_ptbr = "%d/%m/%Y %H:%M %a"
 
 
 print(data_hora_atual.strftime(mascara_ptbr))
