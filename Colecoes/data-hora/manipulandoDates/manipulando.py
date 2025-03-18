da85517eb3978b0dddeeb1fe033edec3aed1d91b
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-03-17 21:06:31 -0300

    Feat: Exemplo utilizando DELTA TIME

---
+++ Colecoes/data-hora/manipulandoDates/manipulando.py
@@ -0,0 +1,12 @@
+# TIME DELTA
+
+# Exemplo
+import datetime
+
+# criando data e hora
+data_hora = datetime.datetime(2025, 3, 17, 21, 5)
+print(data_hora) # 2025-03-17 21:05:00
+
+# adicionando uma semana
+data_hora += datetime.timedelta(weeks=1)
+print(data_hora) # 2025-03-24 21:05:00
