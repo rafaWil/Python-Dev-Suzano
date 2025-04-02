1bd6b6adef79b3126bdf8d63f722aadaa75d4050
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-02 11:30:25 -0300

    Feat: Qual desafio

---
+++ POO/primeiro-projeto-poo/primeiroProgramaPOO.py
@@ -0,0 +1,6 @@
+"""
+Joao tem uma bicicleta e gostaria de registrar as vendas de suas bicicletas.
+Crie um programa onde Joao informe: COR, MODELO, ANO e VALOR da bicicleta vendida. 
+Uma bicicleta pode: BUZINAR, PARAR e CORRRE. 
+Adicione esses comportamentos!
+"""
