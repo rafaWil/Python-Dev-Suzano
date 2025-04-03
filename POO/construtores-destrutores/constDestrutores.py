5831c9df3eb24765c64b8e99db4f20a9dd628941
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-03 11:10:32 -0300

    Fix: Apenas oque é CONSTRUTOR e DESTRUTOR

---
+++ POO/construtores-destrutores/constDestrutores.py
@@ -0,0 +1,11 @@
+# Metodo CONSTRUTOR
+"""
+O método construtor sempre é executado quando uma nova instância da classe é criada. Nesse método inicializamos o estado do nosso objeto. Para declarar o método construtor da classe, criamos um método com o nome __init__.
+
+"""
+
+# Método DESTRUTOR
+"""
+O método destrutor sempre é executado quando uma instância(objeto) é destruída. Destrutores em Python não são tão necessário quanto em C++ porque o Python tem um coletor de lixo que lida com o gerenciamento de memória automaticamente. Para declarar o método destrutor da classe, criamos um método com o nome __del__.
+
+"""
