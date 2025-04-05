e92b34b79faa31ce6bd8e2018974018207810738
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-05 15:45:47 -0300

    Feat: Metodos da classe veiculo

--- POO/heranca/simples.py
+++ POO/heranca/simples.py
@@ -12,13 +12,24 @@ Em programação herança é a capacidade de uma classe filha derivar ou herdar 
 # Itens -> Classe PAI(VEICULO) -> Classes filhos(MOTOCICLETA) - (CARRO) - (CAMINHAO)
 
 class Veiculo:
+    # Atributos
     def __init__(self, cor, placa, numero_rodas):
         self.cor = cor
         self.placa = placa
         self.numero_rodas = numero_rodas
         
-    
+    # Comportamentos/Métodos
+    def ligar_motor(self):
+        print("Ligando o motor...")
+        print("Motor ligado!")
         
+    def desligar_motor(self):
+        print("Desligando o motor...")
+        print("Motor desligado!")
+        
+    def acelerando(self):
+        print("Acelerando...")
+    
 
 
 class Motocicleta(Veiculo):
