9bbefeb5d18dc1addf38e9919602027e2f1d1049
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-05 15:32:55 -0300

    Feat: O que é Heranca

---
+++ POO/heranca/simples.py
@@ -0,0 +1,7 @@
+"""
+HERANÇA
+
+Em programação herança é a capacidade de uma classe filha derivar ou herdar as caraterísticas e comportamentos da classe pai(base).
+
+-> É de natureza transitiva, o que significa que, se a classe B herdar da classe A, todas as subclasses de B herdarão automaticamente da classe A.
+"""
