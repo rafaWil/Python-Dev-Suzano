02745963851baab1a47b854acfcb9857de75042a
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-07 21:39:32 -0300

    Feat: Oque é HERANCA MULTIPLA

---
+++ POO/heranca/multipla.py
@@ -0,0 +1,6 @@
+"""
+Herança múltipla em Python é um conceito da programação orientada a objetos onde uma classe pode herdar de duas ou mais classes ao mesmo tempo.
+
+Ou seja, a classe "filha" (ou derivada) pode acessar os métodos e atributos de múltiplas superclasses.
+
+"""
