2119e46d439615a92fc5ab5f60355e5d428ac449
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-07 21:45:42 -0300

    Feat: Criando Classes

--- POO/heranca/multipla.py
+++ POO/heranca/multipla.py
@@ -4,3 +4,25 @@ Herança múltipla em Python é um conceito da programação orientada a objetos
 Ou seja, a classe "filha" (ou derivada) pode acessar os métodos e atributos de múltiplas superclasses.
 
 """
+# Exemplo Prático
+class Animal:
+    pass
+
+class Mamifero(Animal):
+    pass
+
+class Cachorro(Mamifero):
+    pass
+
+class Gato(Mamifero):
+    pass
+
+class Leao(Mamifero):
+    pass
+
+class Avee(Animal):
+    pass
+
+# Heranca Multipla
+class Ornitorrinco(Mamifero, Ave):
+    pass
