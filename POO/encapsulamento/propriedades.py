70dc8b7ccf25387f88192040d58ba1d58b99a9b5
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-11 15:57:20 -0300

    Feat: Exemplo de propriedades

--- POO/encapsulamento/propriedades.py
+++ POO/encapsulamento/propriedades.py
@@ -3,4 +3,28 @@ PROPERTIES
 
 Com o property() do Python, você pode criar atributos gerenciados em suas classes. Você pode usar atributos gerenciados, também conhecidos como propriedades, quando precisa modificar sua implementação interna sem alterar a API pública da classe.
 
-""" 
+"""
+
+# Propriedades exemplo FOO
+class Foo:
+    def __init__(self, x=None):
+        self._x = x
+        
+    @property
+    def x(self):
+        return self._x or 0
+    
+    @x.setter
+    def x(self, value):
+        self._x = +value
+        
+    @x.deleter
+    def x(self):
+        self._x = -1
+
+foo = Foo(10)
+print(foo.x)
+foo.x = 10
+print(foo.x)
+del foo.x
+print(foo.x) 
