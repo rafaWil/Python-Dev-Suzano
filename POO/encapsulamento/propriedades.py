0427dcb57c1d11644b1611ffd16a503110ad4cb7
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-11 15:42:58 -0300

    Feat: Para que servem os PROPERTIES

---
+++ POO/encapsulamento/propriedades.py
@@ -0,0 +1,6 @@
+"""
+PROPERTIES
+
+Com o property() do Python, você pode criar atributos gerenciados em suas classes. Você pode usar atributos gerenciados, também conhecidos como propriedades, quando precisa modificar sua implementação interna sem alterar a API pública da classe.
+
+""" 
