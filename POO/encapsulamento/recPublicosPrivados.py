7f1f37c609a24b07424cf933ce2f1db85785edfe
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-11 14:21:51 -0300

    Feat: Modificadores de Aceso

---
+++ POO/encapsulamento/recPublicosPrivados.py
@@ -0,0 +1,6 @@
+"""
+Modificadores de Acesso
+
+=> Em linguagens como Java e C++, existem palavras reservadas para definir o nível de acesso aos atributos
+e métodos da classe. Em Python não temos palavras reservadas, porém usamos convenções no nome do recurso, para definir se a variável é pública ou privada.
+"""
