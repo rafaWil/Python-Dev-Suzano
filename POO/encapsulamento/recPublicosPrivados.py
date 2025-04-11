fb92b0c8d69f7c89001633946a931c5ce8ad5307
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-11 15:13:33 -0300

    Feat: Forma correta de se utilizar encapsulamento no exemplo de saldo da conta

--- POO/encapsulamento/recPublicosPrivados.py
+++ POO/encapsulamento/recPublicosPrivados.py
@@ -10,3 +10,21 @@ PRIVADO => Só pode ser acessasdo pela classe.
 Todos os recursos são públicos, a menos que o nome inicie com UNDERLINE. Ou seja, o interpretador Python não irá garantir a proteção do recurso, mas por ser uma convenção amplamente adotada na comunidade, quando encontramos uma variável e/ou método com nome iniciado por UNDERLINE, sabemos que não deveríamos manipular o seu valor diretamente, ou invocar o método fora do escopo da classe.
 
 """
+
+# Exemplo
+class Conta:
+    def __init__(self, saldo=0):
+        self._saldo = saldo
+        
+    def depositar(self, valor):
+        #...
+        self._saldo += valor
+    
+    def sacar(self, valor):
+        #...
+        self._saldo -= valor
+    
+# Instanciando
+conta = Conta(100)
+conta.depositar(100)
+print(conta._saldo)
