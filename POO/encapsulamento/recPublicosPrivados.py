17146e5fda4cb3b3534d5c7de7b9c0d314fca8d8
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-11 14:28:42 -0300

    Feat: Definicoes de PUBLICO & PRIVADO

--- POO/encapsulamento/recPublicosPrivados.py
+++ POO/encapsulamento/recPublicosPrivados.py
@@ -3,4 +3,8 @@ Modificadores de Acesso
 
 => Em linguagens como Java e C++, existem palavras reservadas para definir o nível de acesso aos atributos
 e métodos da classe. Em Python não temos palavras reservadas, porém usamos convenções no nome do recurso, para definir se a variável é pública ou privada.
+
+PÚBLICO => Pode ser acessado de fora da classe.
+PRIVADO => Só pode ser acessasdo pela classe.
+
 """
