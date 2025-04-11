3cd50aaaa2d744cd6c37670007b6ca3f08311f3d
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-11 16:15:40 -0300

    Feat: Exemplo usando LEN

--- POO/polimorfismo/polimorfismo.py
+++ POO/polimorfismo/polimorfismo.py
@@ -3,3 +3,6 @@
 Significa ter muitas formas. Na programação, polimorfismo significa o mesmo nome de função(mas assinaturas diferentes) sendo usado para tipos diferentes.
 
 """
+# Exemplo
+len("python")
+len([10, 20, 30])
