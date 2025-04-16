277c1ac025b813b2cd481b51e1266211f9658d20
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-16 00:12:29 -0300

    Feat: ABC

---
+++ POO/interfacesClasses/Abstratas.py
@@ -0,0 +1,7 @@
+"""
+Criando Classes ABSTRATAS com o módulo ABC
+//ABC
+
+==> Por padrão, o Python não fornece classes abstratas. O Python vem com um módulo que fornece a base para definir as classes abstratas, e o nome do módulo é ABC. O ABC funciona decorando métodos da classe base como abstratos e, em seguida, registrando classes concretas como implementações da base abstrata. Um método se torna abstrato quando decorado com @abstractmethod.
+
+""" 
