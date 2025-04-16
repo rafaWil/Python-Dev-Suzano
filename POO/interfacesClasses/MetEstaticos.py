767b563acc7fda8ee27511c710d96cc545c35f78
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-15 23:13:52 -0300

    Feat: Oque sao Metodos ESTATICOS

---
+++ POO/interfacesClasses/MetEstaticos.py
@@ -0,0 +1,7 @@
+"""
+MÉTODOS ESTÁTICOS
+
+=> Não recebe um primeiro argumento explícito. Ele também é um método vinculado à classe e não ao objeto da classe. 
+Este também não pode acessar ou modificar o estado da classe. Ele está presente em uma classe porque faz sentido que o método esteja presente na classe.
+
+"""
