abc6a367e61d84c095272705fe9fe5b85f6349e3
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-15 23:10:08 -0300

    Feat: Oque é Metodos de CLASSE

---
+++ POO/interfacesClasses/classeEstatico.py
@@ -0,0 +1,7 @@
+"""
+MÉTODOS DE CLASSE
+
+=> Estão ligados à classe e não ao objeto. Eles têm acesso ao estado da classe, pois recebem um parâmetro que aponta para a classe e não para a instância do objeto.
+
+
+"""
