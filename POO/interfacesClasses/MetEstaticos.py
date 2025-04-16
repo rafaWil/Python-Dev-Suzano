14fb2efa3e7ef47d08bc1c3d11d26fabee35e8c0
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-15 23:16:42 -0300

    Feat: Diferencas entre metodos

--- POO/interfacesClasses/MetEstaticos.py
+++ POO/interfacesClasses/MetEstaticos.py
@@ -4,4 +4,11 @@ MÉTODOS ESTÁTICOS
 => Não recebe um primeiro argumento explícito. Ele também é um método vinculado à classe e não ao objeto da classe. 
 Este também não pode acessar ou modificar o estado da classe. Ele está presente em uma classe porque faz sentido que o método esteja presente na classe.
 
+
+DIFERENÇAS
+
+==> Um método de CLASSE recebe um primeiro parâmetro que aponta para a classe, enquanto um método estático não.
+
+==> Um método de CLASSE pode acessar ou modificar o estado da classe enquanto um método estático não pode acessá-lo ou modificá-lo.
+
 """
