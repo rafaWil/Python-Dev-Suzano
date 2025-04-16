4920771f0d443326d1c71ffc056e416d421cb482
Wilson Rafael <rafaelmelo75831@gmail.com>
2025-04-16 00:18:19 -0300

    Feat: Exemplo de Metodo Abstrato

--- POO/interfacesClasses/Abstratas.py
+++ POO/interfacesClasses/Abstratas.py
@@ -5,3 +5,27 @@ Criando Classes ABSTRATAS com o módulo ABC
 ==> Por padrão, o Python não fornece classes abstratas. O Python vem com um módulo que fornece a base para definir as classes abstratas, e o nome do módulo é ABC. O ABC funciona decorando métodos da classe base como abstratos e, em seguida, registrando classes concretas como implementações da base abstrata. Um método se torna abstrato quando decorado com @abstractmethod.
 
 """ 
+# Exemplo
+from abc import ABC, abstractmethod
+
+
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+    
+    @abstractmethod
+    def desligar(self):
+        pass
+    
+class ControleTV(ControleRemoto):
+    def ligar(self):
+        print("TV ligada")
+        
+    def desligar(self):
+        print("TV desligada")
+        
+controle = ControleTV()
+controle.ligar()  # TV ligada
+controle.desligar()  # TV desligada
+
